ca001f1e1cfa06f1da99f367c5679202b629928b
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 21:21:29 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_03_Karenia/Computadora.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03_Karenia
+{
+    class Computadora
+    {
+        int Capacidad;
+        int EspacioDisco;
+        bool Encendido;
+        public Computadora() { }
+        public Computadora(int capacidad, int espacioDisco, bool Encendido)
+        {
+            Capacidad1 = capacidad;
+            EspacioDisco1 = espacioDisco;
+            this.Encendido = Encendido;
+        }
+
+        public int Capacidad1 { get => Capacidad; set => Capacidad = value; }
+        public int EspacioDisco1 { get => EspacioDisco; set => EspacioDisco = value; }
+        public bool Encendido1 { get => this.Encendido; set => this.Encendido = value; }
+
+        public void PreguntarEncendido()
+        {
+            if (this.Encendido)
+            {
+                Console.WriteLine("La computadora no está encendida");
+
+            }
+            else
+            {
+                this.Encendido = true;
+                Console.WriteLine("Bienvenido...");
+            }
+            Console.ReadLine();
+        }
+        public void PreguntarApagado()
+        {
+            Console.WriteLine("¿Quiere apagar la computadora?: ");
+            string c = Console.ReadLine();
+            if (c == "S" || c == "s")
+            {
+                this.Encendido = false;
+                Console.WriteLine("La computadora se apagó");
+            }
+            else
+            {
+                this.Encendido = true;
+                Console.WriteLine("La computadora está encendida");
+            }
+            Console.ReadLine();
+        }
+        public void AnadirDatos(int datos)
+        {
+            if (this.Encendido)
+            {
+                if (datos <= this.Capacidad)
+                {
+                    this.EspacioDisco = this.Capacidad - datos;
+                    Console.WriteLine("Se añadió el archivo {0} y el espacio disponible es: {1}", datos, this.EspacioDisco);
+                }
+                else
+                {
+                    Console.WriteLine("No hay espacio disponible");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La computadora está apagada");
+            }
+        }
+        public void EliminarDato(int datos)
+        {
+            if (this.Encendido)
+            {
+                if (datos <= this.EspacioDisco)
+                {
+                    this.EspacioDisco = this.Capacidad + datos;
+                    Console.WriteLine("El espacio disponible se actualizó: " + this.EspacioDisco);
+                }
+                else
+                {
+                    Console.WriteLine("No hay espacio disponible");
+                }
+            }
+            else
+            {
+                Console.WriteLine("La computadora está apagada");
+            }
+        }
+    }
+}
---
+++ Ejercicio_03_Karenia/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03_Karenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computadora com = new Computadora();
+            com.PreguntarEncendido();
+            if (com.Encendido1)
+            {
+                Console.WriteLine("Digite la capacidad: ");
+                com.Capacidad1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite el espacio disponible: ");
+                com.EspacioDisco1 = int.Parse(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Digite la cantidad de datos a añadir: ");
+                    int n = int.Parse(Console.ReadLine());
+                    com.AnadirDatos(n);
+                    Console.WriteLine("Dite la cantidad de datos a añadir: ");
+                    int m = int.Parse(Console.ReadLine());
+                    com.EliminarDato(m);
+                    com.PreguntarApagado();
+                } while (com.Encendido1);
+
+            }
+        }
+    }
+}
+
